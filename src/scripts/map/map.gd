d562b2e32800a2c8e9974db5a6f1b3ef546bd230
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2026-07-11 16:03:47 +0200

    Update map.gd

--- src/scripts/map/map.gd
+++ src/scripts/map/map.gd
@@ -7,7 +7,7 @@ var element_type = {
 }
 
 
-var itemType = {
+var item_type = {
 	"Seed" = load("res://src/scripts/block/element/sand.tres"),
 	"Tree" = load("res://src/scripts/block/element/dirt.tres"),
 	"Barrier" = load("res://src/scripts/block/element/dirt.tres")
