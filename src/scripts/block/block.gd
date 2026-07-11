830831f57c8207100957a1884e91e5b2776c5f60
ARKANYOTA <arkanyota@icloud.com>
2026-07-11 15:19:22 +0200

    add: structure of code v1

--- src/scripts/block/block.gd
+++ src/scripts/block/block.gd
@@ -7,3 +7,15 @@ class_name Block
 func _init(material, item = null):
 	self.material = material
 	self.item = item
+
+func set_material(new_material):
+	self.material = new_material
+
+func set_item(new_item):
+	self.item = new_item
+
+func on_interact(with = null):
+	return
+	
+func _on_random_tick():
+	return
--- src/scripts/map/map.gd
+++ src/scripts/map/map.gd
@@ -1,13 +1,34 @@
 extends Node2D
 class_name Map
 
-const ressources = {
-	Block.Element.SAND = load("res://src/scripts/block/element/sand.tres")
+var elementType = {
+	"Sand" = load("res://src/scripts/block/element/sand.tres"),
+	"Dirt" = load("res://src/scripts/block/element/dirt.tres")
 }
 
-var map: Dictionary[Vector2i, Block] = {
-	Vector2i(0,0): Block.new(Block.Element.SAND, Block.Item.VOID)
+
+var itemType = {
+	"AH" = load("res://src/scripts/block/element/sand.tres"),
+	"Dirt" = load("res://src/scripts/block/element/dirt.tres")
 }
+
+
+var map: Dictionary[Vector2i, Block] = {
+	Vector2i(0,0): Block.new(elementType["Sand"], null)
+}
+
+func interact_at(pos: Vector2i, picakble: Pickable):
+	# Place item
+	if pos in map.keys():
+		return
+	# Place element
+	else: 
+		return
+
+func place_item(pos: Vector2i, item: String):
+	return
+	
+func place_element(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +38,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+func place_item(item):
