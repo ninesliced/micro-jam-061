6ec3c524181c1252fa613da4455ae41ade54e7eb
ARKANYOTA <arkanyota@icloud.com>
2026-07-11 20:45:23 +0200

    change speed

--- src/scripts/block/item/item.gd
+++ src/scripts/block/item/item.gd
@@ -6,8 +6,8 @@ class_name Item
 
 var current_state = 0
 var water_level = 0
-@export var random_state_update = null
-@export var max_random_state = 15
+@export var random_state_update: float = 0.2
+@export var max_random_state = 4
 
 @export var tileset_source_id = 0
 @export var tileset_atlas_coord = Vector2i(0, 0)
