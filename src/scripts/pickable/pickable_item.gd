03308ad5c9de0378c72a81169a82d84294a1e27e
NowaLeTyrex <nowa.letyrex@gmail.com>
2026-07-11 18:10:55 +0200

    floating pickable_items

--- src/scripts/pickable/pickable_item.gd
+++ src/scripts/pickable/pickable_item.gd
@@ -6,9 +6,13 @@ class_name PickableItem
 @export var is_moving: bool = true
 @export var is_picked: bool = false
 @export var moving_direction: Vector2
-@export var move_speed : float = 2
-var mat : ShaderMaterial
+@export var move_speed : float = 0.1
 
+var mat : ShaderMaterial
+@export var sink_height : int = 4:
+	set(v):
+		sink_height = v
+		set_inline_width(v)
 func _ready() -> void:
 	mat = sprite_2d.material as ShaderMaterial
 	if is_instance_valid(resource): load_resource()
@@ -27,11 +31,13 @@ func pick()-> void:
 func hover()-> void:
 	if not is_picked:
 		set_outline_color(Color.WHITE)
+		set_outline_width(2)
 	
 	
 func unhover()-> void:
 	if not is_picked:
 		set_outline_color(Color.BLACK)
+		set_outline_width(1)
 	
 
 func drop()->void:
@@ -40,3 +46,11 @@ func drop()->void:
 func set_outline_color(color : Color)->void:
 	if mat is ShaderMaterial:
 		mat.set_shader_parameter("outline_color", color)
+
+func set_outline_width(width : int)->void:
+	if mat is ShaderMaterial:
+		mat.set_shader_parameter("outline_width", width)
+
+func set_inline_width(width : int)->void:
+	if mat is ShaderMaterial:
+		mat.set_shader_parameter("inline_width", width)
