c0b56bba67ea779f26b061afd6d971633e935082
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2026-07-11 18:52:55 +0200

    pause menu

---
+++ src/scripts/game/pause_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var resume: Button = $ColorRect/VBoxContainer/Resume
+@onready var quit: Button = $ColorRect/VBoxContainer/Quit
+
+func _ready() -> void:
+	resume.pressed.connect(unpause)
+	quit.pressed.connect(func():
+		get_tree().quit()
+	)
+
+
+func pause():
+	show()
+	get_tree().paused = true
+
+
+func unpause():
+	hide()
+	get_tree().paused = false
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if get_tree().paused:
+			unpause()
+		else:
+			pause()
