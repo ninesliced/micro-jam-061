b13caed1c99e7a66d8de5e4f8c5ca0403109ffee
ARKANYOTA <arkanyota@icloud.com>
2026-07-11 15:56:47 +0200

    place item and element

--- src/scripts/map/map.gd
+++ src/scripts/map/map.gd
@@ -8,8 +8,9 @@ var elementType = {
 
 
 var itemType = {
-	"AH" = load("res://src/scripts/block/element/sand.tres"),
-	"Dirt" = load("res://src/scripts/block/element/dirt.tres")
+	"Seed" = load("res://src/scripts/block/element/sand.tres"),
+	"Tree" = load("res://src/scripts/block/element/dirt.tres"),
+	"Barrier" = load("res://src/scripts/block/element/dirt.tres")
 }
 
 
@@ -17,18 +18,43 @@ var map: Dictionary[Vector2i, Block] = {
 	Vector2i(0,0): Block.new(elementType["Sand"], null)
 }
 
-func interact_at(pos: Vector2i, picakble: Pickable):
-	# Place item
+func is_element_placable(pos) -> bool:
+	# Un element est deja place, docn non
 	if pos in map.keys():
-		return
-	# Place element
-	else: 
-		return
+		return false
+	# faut check si a touche une case a cote
+	for dpos in Utils.four_directions:
+		if pos+dpos in map.keys():
+			return true
+	return false
 
-func place_item(pos: Vector2i, item: String):
+func get_current_block(pos: Vector2i) -> Block: # Ou null
+	if pos in map.keys():
+		return map[pos]
+	return null
+
+func interact_at(pos: Vector2i, picakble: Pickable):
+	# Place element
+	if is_element_placable(pos):
+		match picakble:
+			Pickable.pickableType.Sand:
+				place_item(pos, elementType["Sand"])
+				return
+				
+	# Place item
+	var current_block = get_current_block(pos)
+	if current_block != null:
+		if current_block.item != null:
+			place_item(pos, elementType["Seed"])
+		
+		
+	return
+
+func place_item(pos: Vector2i, item: Item):
 	return
 	
-func place_element(pos)
+func place_element(pos: Vector2i, element: Element):
+	return
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,4 +64,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func place_item(item):
---
+++ src/scripts/utils/utils.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var four_directions = [Vector2i(0, 1), Vector2i(1, 0), Vector2i(-1, 0), Vector2i(0, -1)]
