7c38a9070b0e73ae91db6465e86ff39ada03d4b3
NowaLeTyrex <nowa.letyrex@gmail.com>
2026-07-12 17:44:00 +0200

    change shark sprite

--- src/scripts/map/element_layer.gd
+++ src/scripts/map/element_layer.gd
@@ -1,7 +1,7 @@
 extends TileMapLayer
 
-func block_updated(position: Vector2i, block: Block):
+func block_updated(_position: Vector2i, block: Block):
 	if block and block.element and block.element.tile_map_layer == name:
 		set_cells_terrain_connect([block.position], block.element.terrain_set, block.element.terrain)
 	elif not block:
-		set_cells_terrain_connect([position], 0, -1)
+		set_cells_terrain_connect([_position], 0, -1)
