fe8b30d7f28f0b5385a54dae727abd8611811a80
ARKANYOTA <arkanyota@icloud.com>
2026-07-12 11:55:28 +0200

    BIG BUG FIX

--- src/scripts/block/item/item.gd
+++ src/scripts/block/item/item.gd
@@ -1,13 +1,30 @@
 extends Resource
 class_name Item
 
-@export var name: String
-@export var texture: Texture2D
+var item_name: String
+var texture: Texture2D
 
 var current_state = 0
 var water_level = 0
-@export var random_state_update: float = 0.2
-@export var max_random_state = 4
+var random_state_update: float = 0.2
+var max_random_state = 4
 
-@export var tileset_source_id = 0
-@export var tileset_atlas_coord = Vector2i(0, 0)
+var tileset_source_id = 0
+var tileset_atlas_coord = Vector2i(0, 0)
+
+
+@export var item_resource: ItemResource
+
+func get_value_letter():
+	return item_name[0] + str(current_state)
+
+func _init(item_resource_: ItemResource) -> void:
+	self.item_name = item_resource_.name
+	self.texture = item_resource_.texture
+	current_state = item_resource_.current_state
+	water_level = item_resource_.water_level
+	self.random_state_update = item_resource_.random_state_update
+	self.max_random_state = item_resource_.max_random_state
+	self.tileset_source_id = item_resource_.tileset_source_id
+	self.tileset_atlas_coord = item_resource_.tileset_atlas_coord
+	item_resource = item_resource_
